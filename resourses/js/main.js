63ae235497a4bb1d164cc404f3b64627c02a77c6
PabloSpaniard <pablospaniard@gmail.com>
2017-05-24 21:55:45 +0200

    navbar scroll out added

--- resourses/js/main.js
+++ resourses/js/main.js
@@ -1,12 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.carousel').slick({
         slidesToScroll: 1,
         autoplay: true,
         speed: 1000,
         arrows: false,
         draggable: false,
-        pauseOnFocus:false,
-        pauseOnHover:false
+        pauseOnFocus: false,
+        pauseOnHover: false
+    });
 
+    var lastScrollTop = 0;
+    $(window).scroll(function () {
+        var scrollTop = $(this).scrollTop();
+        if (scrollTop - lastScrollTop > 50) {
+            var navHeight = $('.navbar').css('height');
+            $('.navbar').animate({
+                top: '-' + navHeight
+            }, 150);
+            lastScrollTop = scrollTop;
+        } else if (lastScrollTop - scrollTop > 50) {
+            $('.navbar').animate({
+                top: '0px'
+            }, 150);
+            lastScrollTop = scrollTop;
+        }
     });
 });
