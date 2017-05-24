c15818ee4fdb1c0c92d4e166c8f992bd522441f3
PabloSpaniard <pablospaniard@gmail.com>
2017-05-24 21:44:44 +0200

    courusel added, minor fix for media

---
+++ resourses/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.carousel').slick({
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 1000,
+        arrows: false,
+        draggable: false,
+        pauseOnFocus:false,
+        pauseOnHover:false
+
+    });
+});
